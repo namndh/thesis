4acc4f4c40795b91528a4a2570ffbbebc1db0d67
t3min4l <hainam.nguyen.ict@gmail.com>
2019-05-24 07:44:52 +0700

    update

--- clean.sh
+++ clean.sh
@@ -12,3 +12,11 @@ rm thesis.log
 rm thesis.lot
 rm thesis.out
 rm thesis.toc
+rm -rf main.aux
+rm -rf main.fdb_latexmk
+rm -rf main.fls
+rm -rf main.lof
+rm -rf main.log
+rm -rf main.lot
+rm -rf main.out
+rm -rf main.toc
